ab6296bd0bfad46c7353f2e673f039d54954319f
Alper Bekar <alperbekar05@gmail.com>
2025-12-31 20:57:25 +0100

    refactor: remove unused imports and unnecessary code

--- src/main/java/org/compairifuel/compairifuelapi/health/presentation/HealthController.java
+++ src/main/java/org/compairifuel/compairifuelapi/health/presentation/HealthController.java
@@ -2,8 +2,6 @@ package org.compairifuel.compairifuelapi.health.presentation;
 
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
-import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
 @Path("")
--- src/test/java/org/compairifuel/compairifuelapi/authorization/service/AuthCodeValidatorServiceImplTest.java
+++ src/test/java/org/compairifuel/compairifuelapi/authorization/service/AuthCodeValidatorServiceImplTest.java
@@ -20,7 +20,7 @@ import java.util.HashMap;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
-public class AuthCodeValidatorServiceImplTest {
+class AuthCodeValidatorServiceImplTest {
     private final AuthCodeValidatorServiceImpl sut = new AuthCodeValidatorServiceImpl();
     private SecretKey key;
 
