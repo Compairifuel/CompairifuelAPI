2cf6e511f3496b1b98029c191737fb65867554c0
Jaron Zijlstra <support@jaronline.dev>
2026-01-13 18:22:43 +0100

    refactor: rename ResponseNotOKException to FailedToGetResultsException

---
+++ src/main/java/org/compairifuel/compairifuelapi/utils/service/FailedToGetResultsException.java
@@ -0,0 +1,7 @@
+package org.compairifuel.compairifuelapi.utils.service;
+
+public class FailedToGetResultsException extends RuntimeException {
+    public FailedToGetResultsException(String message) {
+        super(message);
+    }
+}
--- src/main/java/org/compairifuel/compairifuelapi/utils/service/ResponseNotOKException.java
+++
@@ -1,7 +0,0 @@
-package org.compairifuel.compairifuelapi.utils.service;
-
-public class ResponseNotOKException extends RuntimeException {
-    public ResponseNotOKException(String message) {
-        super(message);
-    }
-}
