6b839db244c0479a244c028acf54cdac81ecd58b
Alper Bekar <alperbekar05@gmail.com>
2026-01-20 16:30:11 +0100

    feat: add unit tests for authorization

--- src/main/java/org/compairifuel/compairifuelapi/gasstation/service/domain/GasStationDomain.java
+++ src/main/java/org/compairifuel/compairifuelapi/gasstation/service/domain/GasStationDomain.java
@@ -1,10 +1,12 @@
 package org.compairifuel.compairifuelapi.gasstation.service.domain;
 
 import lombok.Data;
+import lombok.Generated;
 
 import java.util.List;
 
 @Data
+@Generated
 public class GasStationDomain {
     private SummaryDomain summary;
     private List<ResultDomain> results;
